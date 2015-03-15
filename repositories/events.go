53ed0d6ee4dc063183211ae1ee7b885c30e87d52
Carlos <wuiscmc@gmail.com>
2015-03-15 20:08:07 +0100

    Refactored events repository

--- repositories/events.go
+++ repositories/events.go
@@ -1,14 +1,17 @@
 package repositories
 
 import (
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
+	"strconv"
 )
 
 type EventRepository struct{}
 
 func (self EventRepository) LogEvent(adId string, action string) bool {
-	session := getDBSession()
-	_, err := session.Exec("INSERT INTO events VALUES (?,?)", adId, action)
+	db := GetDBSession()
+	idAd, _ := strconv.Atoi(adId)
+	res, err := db.Exec("INSERT INTO events (ad_id, action) VALUES ($1,$2)", idAd, action)
 	checkErr(err)
+  res.LastInsertId()
 	return true
 }
