85b93cb31277882f9f01d3ace247c034b8135287
Carlos <wuiscmc@gmail.com>
2015-03-15 20:01:18 +0100

    Introduced Media struct and changed Id to int The Media struct takes care of the media files information

--- models/ads.go
+++ models/ads.go
@@ -1,9 +1,22 @@
 package models
 
+type Media struct {
+	Type     string
+	Width    int
+	Height   int
+	Delivery string
+	Url      string
+}
+
 type Ad struct {
-	Id          string
+	Id          int
 	Title       string
 	Description string
 	Priority    string
 	ZoneField   string
+	MediaFiles  []Media
+}
+
+func (ad *Ad) SetMediaFiles(mediaFiles []Media) {
+	ad.MediaFiles = mediaFiles
 }
